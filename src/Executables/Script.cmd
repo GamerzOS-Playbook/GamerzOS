1b2aea146f660984a82a4487578faca42992b9e0
MR.PC GAMER <ayanr8096@gmail.com>
2025-08-29 06:25:19 -0700

    Update and rename FPSBOOSTER.cmd to Script.cmd

---
+++ src/Executables/Script.cmd
@@ -0,0 +1,9 @@
+@echo off
+    Start-Process -FilePath "cmd.exe" -ArgumentList "dakstopappinstaller.cmd" -WindowStyle Hidden
+    Start-Process -FilePath "cmd.exe" -ArgumentList "libraries.cmd" -WindowStyle Hidden
+    Start-Process -FilePath "cmd.exe" -ArgumentList "microsoftstore.cmd" -WindowStyle Hidden
+    Start-Process -FilePath "cmd.exe" -ArgumentList "notification.cmd" -WindowStyle Hidden
+    Start-Process -FilePath "cmd.exe" -ArgumentList "storepurchaseapp.cmd" -WindowStyle Hidden
+    Start-Process -FilePath "cmd.exe" -ArgumentList "unpinall.bat" -WindowStyle Hidden
+    Start-Process -FilePath "cmd.exe" -ArgumentList "xbox_identity_provider.cmd" -WindowStyle Hidden
+    regedit.exe /s "RemoveDefender.reg"
