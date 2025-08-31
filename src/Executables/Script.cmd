575ce309440362b0233b54472579b0440d8a9b4a
MR.PC GAMER <ayanr8096@gmail.com>
2025-08-31 21:35:01 +0530

    Update Script.cmd

--- src/Executables/Script.cmd
+++ src/Executables/Script.cmd
@@ -1,105 +1,10 @@
 @echo off
 
-
-:: ----------------------------------------------------------
-:: ---------------Remove "Microsoft Edge" app----------------
-:: ----------------------------------------------------------
-echo --- Remove "Microsoft Edge" app
-PowerShell -ExecutionPolicy Unrestricted -Command "$package = Get-AppxPackage -AllUsers 'Microsoft.MicrosoftEdge'; if (!$package) {; Write-Host 'Not installed'; exit 0; }; $directories = @($package.InstallLocation, "^""$env:LOCALAPPDATA\Packages\$($package.PackageFamilyName)"^""); foreach($dir in $directories) {; if ( !$dir -Or !(Test-Path "^""$dir"^"") ) { continue }; cmd /c ('takeown /f "^""' + $dir + '"^"" /r /d y 1> nul'); if($LASTEXITCODE) { throw 'Failed to take ownership' }; cmd /c ('icacls "^""' + $dir + '"^"" /grant administrators:F /t 1> nul'); if($LASTEXITCODE) { throw 'Failed to take ownership' }; $files = Get-ChildItem -File -Path $dir -Recurse -Force; foreach($file in $files) {; if($file.Name.EndsWith('.OLD')) { continue }; $newName = $file.FullName + '.OLD'; Write-Host "^""Rename '$($file.FullName)' to '$newName'"^""; Move-Item -LiteralPath "^""$($file.FullName)"^"" -Destination "^""$newName"^"" -Force; }; }"
-:: ----------------------------------------------------------
-
-
-
-
-:: ----------------------------------------------------------
-:: ----------Remove Edge through official installer----------
-:: ----------------------------------------------------------
-echo --- Remove Edge through official installer
-reg add "HKLM\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdateDev" /v "AllowUninstall" /t REG_DWORD /d "1" /f
-PowerShell -ExecutionPolicy Unrestricted -Command "$installer = (Get-ChildItem "^""$($env:ProgramFiles)*\Microsoft\Edge\Application\*\Installer\setup.exe"^""); if (!$installer) {; Write-Host 'Installer not found. Microsoft Edge may already be uninstalled.'; } else {; $installer | ForEach-Object {; $uninstallerPath = $_.FullName; $installerArguments = @("^""--uninstall"^"", "^""--system-level"^"", "^""--verbose-logging"^"", "^""--force-uninstall"^""); Write-Output "^""Uninstalling through uninstaller: $uninstallerPath"^""; $process = Start-Process -FilePath "^""$uninstallerPath"^"" -ArgumentList $installerArguments -Wait -PassThru; if ($process.ExitCode -eq 0 -or $process.ExitCode -eq 19) {; Write-Host "^""Successfully uninstalled Edge."^""; } else {; Write-Error "^""Failed to uninstall, uninstaller failed with exit code $($process.ExitCode)."^""; }; }; }"
-:: ----------------------------------------------------------
-
-
-
-:: ----------------------------------------------------------
-:: ------------------Remove Edge shortcuts-------------------
-:: ----------------------------------------------------------
-echo --- Remove Edge shortcuts
-PowerShell -ExecutionPolicy Unrestricted -Command "$shortcuts = @(; @{ Revert = $True;  Path = "^""$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk"^""; }; @{ Revert = $True;  Path = "^""$env:AppData\Microsoft\Internet Explorer\Quick Launch\Microsoft Edge.lnk"^""; }; @{ Revert = $True;  Path = "^""$env:AppData\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Microsoft Edge.lnk"^""; }; @{ Revert = $True;  Path = "^""$env:Public\Desktop\Microsoft Edge.lnk"^""; }; @{ Revert = $True;  Path = "^""$env:SystemRoot\System32\config\systemprofile\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\Microsoft Edge.lnk"^""; }; @{ Revert = $False; Path = "^""$env:UserProfile\Desktop\Microsoft Edge.lnk"^""; }; ); foreach ($shortcut in $shortcuts) {; if (-Not (Test-Path $shortcut.Path)) {; Write-Host "^""Skipping, shortcut does not exist: `"^""$($shortcut.Path)`"^""."^""; continue; }; try {; Remove-Item -Path $shortcut.Path -Force -ErrorAction Stop; Write-Output "^""Successfully removed shortcut: `"^""$($shortcut.Path)`"^""."^""; } catch {; Write-Error "^""Encountered an issue while attempting to remove shortcut at: `"^""$($shortcut.Path)`"^""."^""; }; }"
-:: ----------------------------------------------------------
-
-del "%windir%\Program Files (x86)\Internet Explorer" /s /f /q
-taskkill /f /t /IM MicrosoftEdgeUpdate.exe
-del "%windir%\Program Files (x86)\Microsoft" /s /f /q
-taskkill /f /t /IM MicrosoftEdgeUpdate.exe
-del "%windir%\Windows\SystemApps Microsoft.MicrosoftEdge" /s /f /q
-del "%windir%\Program Files\Internet Explorer" /s /f /q
-del "%windir%\Windows\bcastdvr" /s /f /q
-taskkill /f /t /IM GameBarPresenceWriter.exe
-del "%windir%\Windows\GameBarPresenceWriter" /s /f /q
-taskkill /f /t /IM CompPkgSrv.exe
-del "%windir%\Windows\System32\CompatTelRunner.exe" /s /f /q
-taskkill /f /t /IM upfc.exe
-del "%windir%\Windows\System32\upfc.exe" /s /f /q
-del "%windir%\Windows\System32\CompPkgSrv.exe" /s /f /q
-taskkill /f /t /IM mobsync.exe
-del "%windir%\Windows\System32\mobsync.exe" /s /f /q
-taskkill /f /t /IM smartscreen.exe
-del "%windir%\Windows\System32\smartscreen.exe" /s /f /q
-taskkill /f /t /IM GameBarPresenceWriter.exe
-del "%windir%\Windows\System32\GameBarPresenceWriter" /s /f /q
-del "%windir%\Users\%username%\AppData\Local\Microsoft\GameDVR" /s /f /q
-taskkill /f /t /IM MicrosoftEdgeUpdate.exe
-del "%windir%\Users\%username%\AppData\Local\Microsoft\Edge" /s /f /q
-taskkill /f /t /IM StartMenuExperienceHost.exe
-taskkill /f /t /IM ScreenClippingHost.exe
-del "%windir%\Windows\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\StartMenuExperienceHost.exe" /s /f /q
-taskkill /f /t /IM TextInputHost.exe
-del "%windir%\Windows\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\TextInputHost.exe" /s /f /q
-rmdir /S /Q "%windir%\Users\%username%\AppData\Local\Microsoft\GameDVR"
-taskkill /f /t /IM MicrosoftEdgeUpdate.exe
-rmdir /S /Q "%windir%\Users\%username%\AppData\Local\Microsoft\Edge"
-rmdir /S /Q "%windir%\Program Files (x86)\Internet Explorer"
-rmdir /S /Q "%windir%\Program Files (x86)\Microsoft"
-rmdir /S /Q "%windir%\Windows\SystemApps Microsoft.MicrosoftEdge"
-
-
-
-
-echo -- Debloating Edge
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "EdgeEnhanceImagesEnabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "PersonalizationReportingEnabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "ShowRecommendationsEnabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "HideFirstRunExperience" /t REG_DWORD /d 1 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "UserFeedbackAllowed" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "ConfigureDoNotTrack" /t REG_DWORD /d 1 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "AlternateErrorPagesEnabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "EdgeCollectionsEnabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "EdgeFollowEnabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "EdgeShoppingAssistantEnabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "MicrosoftEdgeInsiderPromotionEnabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "RelatedMatchesCloudServiceEnabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "ShowMicrosoftRewards" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "WebWidgetAllowed" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "MetricsReportingEnabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "StartupBoostEnabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "BingAdsSuppression" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "NewTabPageHideDefaultTopSites" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "PromotionalTabsEnabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "SendSiteInfoToImproveServices" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "SpotlightExperiencesAndRecommendationsEnabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "DiagnosticData" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "EdgeAssetDeliveryServiceEnabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "CryptoWalletEnabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "WalletDonationEnabled" /t REG_DWORD /d 0 /f
-echo -- Uninstalling Edge
-powershell -NoProfile -ExecutionPolicy Bypass -Command "$script = (New-Object Net.WebClient).DownloadString('https://cdn.jsdelivr.net/gh/he3als/EdgeRemover@main/get.ps1'); $script = [ScriptBlock]::Create($script); & $script -UninstallEdge"
-
+powershell -WindowStyle Hidden -Command "Start-Process powershell -ArgumentList '-File \"EdgeRemover.ps1\" -ExecutionPolicy Bypass' -Verb RunAs -WindowStyle Hidden"
 PowerPoint.cmd >NUL 2>NUL
 autohide.cmd >NUL 2>NUL
 notification.cmd >NUL 2>NUL
-edge.cmd >NUL 2>NUL
 unpinall.cmd >NUL 2>NUL
 regedit.exe /s "RemoveDefender.reg"
 
-exit
+pause
