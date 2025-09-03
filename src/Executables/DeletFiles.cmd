50f9e99d7651092a862b522abc3af83d7f71077e
MR.PC GAMER <ayanr8096@gmail.com>
2025-09-03 17:44:36 +0530

    Add files via upload

---
+++ src/Executables/DeletFiles.cmd
@@ -0,0 +1,15 @@
+@echo off
+:: Check for administrative privileges
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~f0' -Verb runAs"
+    exit /b
+)
+
+:: Actual commands
+rmdir /s /q "%WINDIR%\GamerzOSModules"
+rmdir /s /q "%WINDIR%\GamerzOSDesktop"
+
+echo Deletion complete.
+pause
